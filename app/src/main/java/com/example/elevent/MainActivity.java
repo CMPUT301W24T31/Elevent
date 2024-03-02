36a92f93f5f3e5c06856b3877b899c82297011d3
Andrew Zhang <azhang7@ualberta.ca>
2024-03-01 19:06:51 -0700

    created GenerateQRCodeActivity

--- app/src/main/java/com/example/elevent/MainActivity.java
+++ app/src/main/java/com/example/elevent/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.elevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button test = findViewById(R.id.test_button);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GenerateQRCodeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
