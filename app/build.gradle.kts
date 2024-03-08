d75e0634d8e6d9c7d9a1cc9258c410629ba9d1ac
kidus-der <101529458+kidus-der@users.noreply.github.com>
2024-03-07 23:14:26 -0700

    trying to solve merge conflicts with main
    solving merge conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,9 @@ android {
 dependencies {
 
     // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:32.7.3")) // BoM for Firebase as a whole
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
     implementation("com.google.firebase:firebase-database")
-    implementation("com.google.firebase:firebase-firestore:24.10.3") // dependency for firestore
-    implementation("com.google.firebase:firebase-storage") // dependency for cloud storage used for images
-
+    implementation("com.google.firebase:firebase-firestore:24.10.3")
 
 
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -52,6 +50,7 @@ dependencies {
     implementation("com.google.zxing:core:3.5.3")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("androidx.preference:preference:1.2.0")
+    implementation("androidx.navigation:navigation-runtime:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
